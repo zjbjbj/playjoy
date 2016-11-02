c0dedcd87adb8db29e65726bd68996bd4de4318c
jzbjbj <jzbjbj@163.com>
2016-11-02 17:12:11 +0800

    Administrator

---
+++ src/main/java/com/playjoy/base/servlet/DruidStatFilter.java
@@ -0,0 +1,22 @@
+package com.playjoy.base.servlet;
+
+/**
+ * druid过滤器.
+ * @author Administrator
+ *
+ */
+
+
+import com.alibaba.druid.support.http.WebStatFilter;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+@WebFilter(filterName="druidWebStatFilter",urlPatterns="/*",
+        initParams={
+                @WebInitParam(name="exclusions",value="*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")// 忽略资源
+        }
+)
+public class DruidStatFilter extends WebStatFilter {
+
+        }
